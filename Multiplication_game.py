72d97b73aada9bee1302b4ee628311cebe35b954
Sai-Sudha-Amirikunta <55869334+Sai-Sudha-Amirikunta@users.noreply.github.com>
2020-02-08 19:15:01 +0530

    Update Multiplication_game.py

--- Multiplication_game.py
+++ Multiplication_game.py
@@ -1,9 +1,9 @@
 
 #Question
 
-#Write a multiplication game program for kids. The program should give the player ten randomly generated multiplication questions to do. 
-#After each, the program should tell them whether they got it right or wrong and what the correct answer is and finally it should display 
-#the total score out of 10 scored by user.
+'''Write a multiplication game program for kids. The program should give the player ten randomly generated multiplication questions to do. 
+After each, the program should tell them whether they got it right or wrong and what the correct answer is and finally it should display 
+the total score out of 10 scored by user.'''
 
 
 import random 
